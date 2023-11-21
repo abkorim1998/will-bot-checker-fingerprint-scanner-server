1e4457ed663ad70683c46a1347dd5d70ce07a50a
abkorim1998 <abkorim1998@gmail.com>
2023-11-21 13:21:15 +0600

    new update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const scanner = require('fpScanner');
+const scanner = require('fpscanner');
 const cors = require('cors');
 
 const app = express();
