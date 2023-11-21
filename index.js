32d5e98ada6b71e15293236626aba47732034659
abkorim1998 <abkorim1998@gmail.com>
2023-11-21 12:58:22 +0600

    new update

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const scanner = require('fpScanner');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Enable CORS for all routes
+// Parse incoming request bodies in a middleware before your handlers, available under req.body
+app.use(bodyParser.json());
+
+app.post('/handle-post', (req, res) => {
+    const requestBody = req.body;
+    try {
+        //fingerprint is the fingerprint collected with fp-collect
+        scannerResults = scanner.analyseFingerprint(requestBody);
+        // User agent of the browser
+        res.json({ message: 'result', scannerResults });
+    } catch (error) {
+        res.json({ message: 'result', undefined });
+    }
+});
+
+app.get('/', (req, res)=> {
+    res.json({ hi: "it's runing"})
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
